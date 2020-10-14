ef4993192c4cb37ab24706415cde8b2151f46173
Lusine Baghdasaryan <lusinebaghdasaryan1994@gmail.com>
2020-10-14 05:36:42 +0400

    ToDo

---
+++ todo-list-react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import A from './demo/A';
+import Todo from './components/ToDo';
+
+
+function App() {
+
+  return (
+   <Todo />
+  );
+}
+
+export default App;
